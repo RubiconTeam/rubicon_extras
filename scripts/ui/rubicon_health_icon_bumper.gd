dff9fa38075aae351b7623b73138f548863ee3c8
legole0 <legole0@proton.me>
2026-08-20 02:15:37 -0300

    HealthIconBumper: WIP Functionality

--- scripts/ui/rubicon_health_icon_bumper.gd
+++ scripts/ui/rubicon_health_icon_bumper.gd
@@ -15,7 +15,30 @@ enum BumpTime {
 		rubicon_level = value
 		set_bump_time(bump_every)
 		
-@export var icons_to_bump: Array[Node2D]
+
+@export var icon_group_first: Node2D
+@export var icon_group_second: Node2D
+#@export var icons_to_bump: Array[Node2D]:
+	#set(value):
+		#if value == icons_to_bump or value == null:
+			#return
+		#
+		#icons_to_bump = value
+		#for i: int in icons_to_bump.size():
+			#var icon: Node2D = icons_to_bump[i]
+			#if icon == null:
+				#_icons_base_scale[i] = Vector2.ZERO
+				#continue
+				#
+			#_icons_base_scale[i] = icon.scale
+		#notify_property_list_changed()
+		#
+		#var icon_amount: int = icons_to_bump.size()
+		#for i: int in _icons_base_scale.size():
+			#if i < icon_amount:
+				#continue
+			#else:
+				#_icons_base_scale.remove_at(i)
 
 @export var bump_every: BumpTime = BumpTime.MEASURE:
 	set(value):
@@ -24,11 +47,12 @@ enum BumpTime {
 
 @export_range(1, 128) var bump_interval: int = 1
 
-@export_custom(PROPERTY_HINT_LINK, "") var bump_amount: Vector2 = Vector2(0.05, 0.05)
+#@export_custom(PROPERTY_HINT_LINK, "") var bump_amount: Vector2 = Vector2(0.05, 0.05)
+@export_custom(PROPERTY_HINT_LINK, "") var bump_amount_first: Vector2 = Vector2(0.05, 0.05)
+@export_custom(PROPERTY_HINT_LINK, "") var bump_amount_second: Vector2 = Vector2(-0.05, 0.05)
 @export var bump_interpolate_speed: float = 3.125
 
-var _icon_property_list: PackedStringArray = []
-@export_storage var _icon_property_value: Dictionary[StringName, Vector2]
+@export_storage var _icons_base_scale: Array[Vector2]
 
 func set_bump_time(new_bump_time: BumpTime):
 	if rubicon_level == null:
@@ -73,16 +97,27 @@ func icon_bump() -> void:
 	if cur_time % bump_interval != 0:
 		return
 	
-	for icon in icons_to_bump:
-		icon.scale += bump_amount
+	#for icon in icons_to_bump:
+		#icon.scale += bump_amount
+	if icon_group_first != null:
+		icon_group_first.scale += bump_amount_first
+	if icon_group_second != null:
+		icon_group_second.scale += bump_amount_second
 
-var placeholder_icon_base_scale: Vector2 = Vector2.ONE
+var placeholder_icon_base_scale_first: Vector2 = Vector2.ONE
+var placeholder_icon_base_scale_second: Vector2 = Vector2(-1, 1)
 func _process(delta: float) -> void:
-	if !enabled:
+	if !enabled or rubicon_level == null:
 		return
 	
-	for icon in icons_to_bump:
-		icon.scale = icon.scale.lerp(placeholder_icon_base_scale, bump_interpolate_speed * delta)
+	#for icon in icon_groups_to_bump:
+		#icon.scale = icon.scale.lerp(placeholder_icon_base_scale, bump_interpolate_speed * delta)
+	# this approach is extremely limited, i'll be adding a proper way of doing this,
+	# i just want to finish this atm
+	if icon_group_first != null:
+		icon_group_first.scale = icon_group_first.scale.lerp(placeholder_icon_base_scale_first, bump_interpolate_speed * delta)
+	if icon_group_second != null:
+		icon_group_second.scale = icon_group_second.scale.lerp(placeholder_icon_base_scale_second, bump_interpolate_speed * delta)
 
 func get_cur_time_value() -> float:
 	match bump_every:
@@ -92,50 +127,28 @@ func get_cur_time_value() -> float:
 			return rubicon_level.clock.time_step
 	return rubicon_level.clock.time_measure
 
-func _get_property_list() -> Array[Dictionary]:
-	var properties: Array[Dictionary] = []
-	
-	if !icons_to_bump.is_empty():
-		properties.append({
-			name = &"Icon Base Scale",
-			type = TYPE_NIL,
-			usage = PROPERTY_USAGE_GROUP,
-			hint_string = "scale_",
-		})
-		
-		_icon_property_list.clear()
-		for icon: AnimatedSprite2D in icons_to_bump:
-			_icon_property_list.append(icon.name)
-			var icon_property_name: StringName = &"scale_%s_base_scale" % [icon.name]
-			properties.append({
-				name = icon_property_name,
-				type = TYPE_VECTOR2,
-				usage = PROPERTY_USAGE_DEFAULT,
-				hint = PROPERTY_HINT_LINK
-			})
-			_icon_property_value.set(icon_property_name, icon.scale)
-			#print(get(icon_property_name))
-		#print(_icon_property_list,_icon_property_value)
-		
-		# prevent deleted values to be saved in the scene making it infinitely bloated
-		for value_key:StringName in _icon_property_value.keys():
-			if !_icon_property_list.has(value_key):
-				_icon_property_value.erase(value_key)
-	
-	return properties
-
-func _get(property: StringName) -> Variant:
-	if property.begins_with("scale_"):
-		if _icon_property_list.has(property):
-			#print("get property "+property)
-			return _icon_property_value[property]
-	
-	return null
-
-func _set(property: StringName, value: Variant) -> bool:
-	if property.begins_with("scale_"):
-		if _icon_property_list.has(property):
-			_icon_property_value.set(property, value)
-			#print("set property "+property)
-			return true
-	return false
+#func _get_property_list() -> Array[Dictionary]:
+	#var properties: Array[Dictionary] = []
+	#
+	#if !icons_to_bump.is_empty():
+		#properties.append({
+			#name = &"Icon Base Scale",
+			#type = TYPE_NIL,
+			#usage = PROPERTY_USAGE_GROUP,
+			#hint_string = "scale_",
+		#})
+		#
+		#for icon: AnimatedSprite2D in icons_to_bump:
+			#if icon == null:
+				#continue
+			#var icon_property_name: StringName = &"scale_%s_base_scale" % [icon.name]
+			#properties.append({
+				#name = icon_property_name,
+				#type = TYPE_VECTOR2,
+				#usage = PROPERTY_USAGE_DEFAULT,
+				#hint = PROPERTY_HINT_LINK
+			#})
+		#
+		#
+	#
+	#return properties
